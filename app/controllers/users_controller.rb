d756049b4145e5c91fd94e0cab6b834c942d55f0
anjarupnik <anjarupnik@hotmail.com>
2017-10-23 13:20:14 +0200

    admins and users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+
+    def index
+
+    end
+
+    def show
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :admins
   devise_for :users
+  resources :users
+  resources :admins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
