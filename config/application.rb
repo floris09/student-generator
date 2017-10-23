6eb12ada47abfe201a7520a250fc2cb13990bb70
floris09 <florismeininger@gmail.com>
2017-10-23 11:10:39 +0200

    add gems and install

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module StudentGenerator
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    console do
+      ActiveRecord::Base.connection
+    end
   end
 end
