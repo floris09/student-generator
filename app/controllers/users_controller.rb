8edb206cab6e3408d536bfc207c480dd9de05390
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-10-23 16:00:21 +0200

    Promote and Demote Admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :find_user, only: [:edit, :update]
+  before_action :user_params, only: [:update]
 
     def index
 
     end
 
     def show
+    end
+
+    def edit
+    end
+
+    def update
+    if @user.update_attributes(user_params)
+      redirect_to admins_path
+    else
+      render 'edit'
+    end
+  end
+
+    private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:admin)
     end
 
 end
