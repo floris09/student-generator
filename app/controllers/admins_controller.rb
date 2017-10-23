ae1017ef5dbb0928c7ec1d4de7614823adfc96c3
anjarupnik <anjarupnik@hotmail.com>
2017-10-23 15:20:39 +0200

    pairs coming together

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,8 +11,13 @@ class AdminsController < ApplicationController
   def showpair
     @users = User.all
     @students = @users.select {|a| a.not_admin?}
+    @pairs = []
+    i = @students.count
+    while i > 0 do
     get_student
-
+    @pairs << @pair
+    i -= 1
+    end
   end
 
   def show
@@ -27,7 +32,9 @@ class AdminsController < ApplicationController
     while @student1 == @student2
       @student1 = @students.sample
     end
-
+    @pair = []
+    @pair << @student1.email
+    @pair << @student2.email
   end
 
 end
