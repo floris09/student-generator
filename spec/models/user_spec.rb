b91e7532b351f31322639d36022cbcad87390971
anjarupnik <anjarupnik@hotmail.com>
2017-10-25 13:23:47 +0200

    tests for user methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#admin?" do
+  let(:user) {create :user, admin: true}
+
+   it "returns true if user is admin" do
+     expect(user.admin?).to eq(true)
+   end
+ end
+
+ describe "#not_admin?" do
+ let(:user) {create :user}
+
+  it "returns true if user is admin" do
+    expect(user.admin?).to eq(false)
+  end
+end
+
+
+
+
 end
