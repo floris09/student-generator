0f1effd69592b287be74cdd9078fc318c5e494a9
floris09 <florismeininger@gmail.com>
2017-10-25 13:59:43 +0200

    add feature tests for admin and view test for navbar

--- spec/feature/manage_admins_spec.rb
+++ spec/feature/manage_admins_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature 'Manage admins', js: true do
   let!(:admin) { create :user, admin: true }
-  let!(:student) { create :user, admin:false, id: '1000' }
+  let!(:student) { create :user, admin: false, id: '1000' }
+  let!(:admin2) { create :user, admin: true, id: '2000' }
 
   scenario 'make a new admin' do
 
@@ -15,11 +16,24 @@ feature 'Manage admins', js: true do
     visit admins_path
 
     page.find("#toggle-admin-1000").click
-    # page.execute_script("promoteAdmin(1000)")
-
-    sleep(1)
 
     expect(page.find("#toggle-admin-1000")).to have_content("Make Student")
+
+  end
+
+  scenario 'demote to student' do
+
+    visit new_user_session_path
+
+    fill_in "user_email", :with => admin.email
+    fill_in "user_password", :with => "123456"
+    click_button "Log in"
+
+    visit admins_path
+
+    page.find("#toggle-admin-2000").click
+
+    expect(page.find("#toggle-admin-2000")).to have_content("Make Admin")
 
   end
 end
