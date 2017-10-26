8d7a998cc5909e555e6a5f2fd4a8c895d6c044e4
anjarupnik <anjarupnik@hotmail.com>
2017-10-26 13:46:16 +0200

    odd numbers round robin fixed

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -45,9 +45,13 @@ private
       s = @student_names.dup
 
       for i in 1..(@students.count/2) do
+        if s.count == 3
+          @day << [s[0], "#{s[1]} + #{s[2]}"]
+        else
           @day << [s.first,s.last]
           s.slice!(0)
           s.slice!(s.count-1)
+        end
       end
 
       @pairs << @day
